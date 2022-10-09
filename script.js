71de0b6eca27a28c97c8bfa3ca90e741046b6946
Cheese Allergy Hamster <shlifedev@gmail.com>
2022-10-10 00:16:18 +0900

    blocking으로 예상되는 지점에서 자동 재시작

--- script.js
+++ script.js
@@ -1,22 +1,49 @@
 let elements = document.querySelectorAll(`.xDtZAf`);  
+const wait = (t) => new Promise((resolve )=>{ setTimeout(()=>{resolve()}, t) })
 const deleteComment = async (element) => {   
   const button = element.querySelector('button'); 
   button.click();
-  return new Promise((resolve)=>{
+  return new Promise(async (resolve, reject)=>{
+    let blcoked = false;
+    let intervalCount = 0;
     const interval = setInterval(()=>{
       const newElements = document.querySelectorAll(`.xDtZAf`);  
+      
       if(elements.length !== newElements.length){
          resolve();
          clearInterval(interval);
          elements = newElements;
+      
+        }
+      else{
+        if(newElements.length === 0 ) return;
+        intervalCount ++;
+        if(intervalCount === 50) // 5s 
+          blocked = true;
+        if(blocked){ 
+          intervalCount = 0;
+          blocked = false;
+          console.log('maybe blocked, re-start process')    
+          elements = newElements;
+          reject();
+        }
       }
     }, 100);
   });
 } 
-const proccess = async () =>{
-  for(let el of elements){ 
-    await deleteComment(el);
+const process = async () =>{
+  try{
+    for(let el of elements){ 
+      console.log('delete comment...'); 
+      await deleteComment(el);
+      console.log('deleted, wait for prevent blocking');
+      await wait(2000);
+    }
   }
+  catch{
+    process();
+  }
+ 
 }
 
-proccess();
+process();
